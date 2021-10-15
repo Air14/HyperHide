cabf2c73419ba0e72889f6ade01b55003abe11a2
共产主义接班人 <503407184@qq.com>
2021-10-15 10:48:29 +0800

    Update create.bat
    update cp system driver folder
    fix system drive

--- Scripts/create.bat
+++ Scripts/create.bat
@@ -1,3 +1,7 @@
-sc create airhv type= Kernel binpath= C:\Windows\system32\drivers\airhv.sys
-sc create HyperHideDrv type= Kernel binpath= C:\Windows\system32\drivers\HyperHideDrv.sys
+cd %~dp0
+cd ..\
+copy airhv.sys %SystemRoot%\system32\drivers\airhv.sys
+copy HyperHideDrv.sys %SystemRoot%\system32\drivers\airhv.sys
+sc create airhv type= Kernel binpath= %SystemRoot%\system32\drivers\airhv.sys
+sc create HyperHideDrv type= Kernel binpath= %SystemRoot%\system32\drivers\HyperHideDrv.sys
 pause
